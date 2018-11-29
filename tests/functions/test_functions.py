fd1bf9f4a7b9f90890495924cc779eabf38882b7
Finnkauski <iwiivi@gmail.com>
2018-11-29 12:36:27 +0000

    feature: added a basic test for compose

--- tests/functions/test_functions.py
+++ tests/functions/test_functions.py
@@ -1,10 +1,21 @@
 
 #-- Imports ---------------------------------------------------------------------
+# base
+import functools
+
+# third party
 import pytest
 from woffle.functions.compose import *
 
+from hypothesis import given
+from hypothesis import strategies as st
+
+#-- Fixtures & Definitions ------------------------------------------------------
 
 #-- Tests -----------------------------------------------------------------------
 def test_id():
     assert id(1) is 1
 
+@given(st.lists(st.integers()))
+def test_basic_composition(integers):
+    assert compose(str, sum, functools.partial(map, int))(integers) == str(sum(map(int,integers)))
